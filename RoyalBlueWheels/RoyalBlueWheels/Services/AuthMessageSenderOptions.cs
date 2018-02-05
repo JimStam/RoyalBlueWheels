629b74ef5f12e2a4179f7bdbe86ecdcd91654423
Jim Stam <jimstam@hotmail.com>
2018-02-05 12:26:02 +0100

    Website upload

---
+++ RoyalBlueWheels/RoyalBlueWheels/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebshopHPWcore.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser = "";
+        public string SendGridKey = "";
+    }
+}
